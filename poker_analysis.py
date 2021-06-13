a5e0a21faaaef46dff6979710722655daa2b46df
ChrisW-priv <58006971+ChrisW-priv@users.noreply.github.com>
2021-06-13 19:10:49 +0200

    code refactor done, ready to implement asyncio + cython

--- poker_analysis.py
+++ poker_analysis.py
@@ -1,5 +1,5 @@
-from collections import Counter
 from itertools import combinations
+from time import time
 
 
 class PokerEvaluator:
@@ -40,174 +40,118 @@ class PokerEvaluator:
 
 		for table_addition in combinations(self.deck, 5-len(self._table)):
 			full_table = self._table.union(table_addition)
-			deck = self.deck - set(table_addition)
-			our_cards_value = self.max_value_of_players_hand(full_table, self._hand)
-			for enemy_cards in combinations(deck, 2):
-				enemy_cards_value = self.max_value_of_players_hand(full_table, enemy_cards)
+			our_cards_value = self.value_of_players_hand(full_table, self._hand)
+
+			rest_of_deck = self.deck - set(table_addition)
+			for enemy_cards in combinations(rest_of_deck, 2):
+				enemy_cards_value = self.value_of_players_hand(full_table, enemy_cards)
 				if our_cards_value > enemy_cards_value:
 					win += 1
 				elif our_cards_value == enemy_cards_value:
 					draw += 1
 				else:
 					loss += 1
-					print(enemy_cards, our_cards_value, enemy_cards_value)
 
-		print(win, draw, loss)
+		return win, draw, loss
 
-	def max_value_of_players_hand(self, base, hand):
+	def value_of_players_hand(self, base, hand):
 		both_combined = base.union(hand)
-		max_ = 0
+		hand_type, val, highest_card = self.best_hand_type_from_seven_cards(both_combined)
+		# hand type is an int representing index of type
+		# if hand type is strit, kolor, full or poker we do not add the highest card
+		count_highest_card = hand_type not in {4, 5, 6, 8}
+		return 1000 * hand_type + 10 * val + highest_card * count_highest_card
 
-		for cards in combinations(both_combined, 5):
-			val = self.value_of_five_cards(cards)
-			max_ = max(max_, val)
-
-		return max_
-
-	def get_hand_type_of_five_cards(self, cards):
-		def check_consecutive_len5(l):
+	def best_hand_type_from_seven_cards(self, cards7):
+		def check_consecutive_len5(cards5):
 			"""checks if ordered cards are incrementing by one"""
-			what_it_would_be = list(range(min(l), max(l) + 1))
-			return sorted(l) == what_it_would_be and len(what_it_would_be) == 5
+			sorted_l = sorted(cards5)
+			consecutive_len5 = list(range(sorted_l[0], sorted_l[0] + 6))
+			return sorted_l == consecutive_len5
+
+		def check_if_colour_in_cards(cards):
+			colours = (colour for _, colour in cards)
+			for colour in self.COLOURS:
+				c = sum(colour == col for col in colours)
+				if c == 5:
+					return True
 
 		# we make a list of all cards as numbers based on their value
-		main = [self.CARD_VALUES[number] for number, _ in cards]
-		# if there is an ase we have to look for two separate cases
-		secondary = [] if 14 not in main else [1 if x == 14 else x for x in main]
+		numbers = [self.CARD_VALUES[number] for number, _ in cards7]
+		highest_card = max(numbers)
 
-		highest_card = max(main)
+		para, dwie_pary, trojka, strit, kolor, full, kareta, poker = range(1, 9)  # indexes of hand types
+		hand_types_on_table = [False for _ in self.HAND_VALUES]
+		hand_types_on_table[0] = True
+		value_of_type = [0 for _ in self.HAND_VALUES]
 
-		# we check if there are five cards of the same colour
-		colours_on_table = (colours for _, colours in cards)
-		there_is_colour_of_type = (all(col == colour for col in colours_on_table) for colour in self.COLOURS)
-		colour_on_table = any(there_is_colour_of_type)
+		def update_hand_type_on_table(hand_type_index, value_of_hand_type):
+			nonlocal hand_types_on_table, value_of_type
+			hand_types_on_table[hand_type_index] = True
+			value_of_type[hand_type_index] = max(value_of_hand_type, value_of_type[hand_type_index])
 
-		"""We check if there is strit or poker on table"""
-		if check_consecutive_len5(main):
-			if colour_on_table:
-				return 'poker', highest_card, highest_card
+		# check if poker or strit
+		for combination_of5 in combinations(cards7, 5):
+			main = [self.CARD_VALUES[number] for number, _ in combination_of5]
+			max_main = max(main)
+
+			# if there is an ase we have to look for two separate cases
+			secondary = [] if 14 not in main else [1 if x == 14 else x for x in main]
+
+			if check_if_colour_in_cards(combination_of5):
+				update_hand_type_on_table(kolor, max_main)
+				if check_consecutive_len5(main):
+					update_hand_type_on_table(poker, max_main)
+				elif secondary and check_consecutive_len5(secondary):
+					update_hand_type_on_table(poker, max(secondary))
 			else:
-				return 'strit', highest_card, highest_card
-		if secondary:
-			"""we need to check if there is strit or poker on table provided we count the ase as 1 (if ase on table)"""
-			if check_consecutive_len5(secondary):
-				if colour_on_table:
-					return 'poker', max(secondary), highest_card
-				else:
-					return 'strit', max(secondary), highest_card
+				if check_consecutive_len5(main):
+					update_hand_type_on_table(strit, max_main)
+				elif secondary and check_consecutive_len5(secondary):
+					update_hand_type_on_table(strit, max(secondary))
 
-		"""if there are 5 cards of the same colour and method has not terminated by this point 
-		we return 'colour' as the highest type of hand one can have'"""
-		if colour_on_table:
-			return 'kolor', highest_card, highest_card
+		for number in set(numbers):
+			count = numbers.count(number)
 
-		type_of_hand = 'highest_card'
-		value_of_hand = 0
-
-		for number in main:
-			count = main.count(number)
-
-			if count == 2:
-				if type_of_hand == 'para':
-					return 'dwie_pary', value_of_hand + number, highest_card
-				elif type_of_hand == 'trojka':
-					return 'full', value_of_hand * 3 + number * 2, highest_card
-				else:
-					type_of_hand = 'para'
-					value_of_hand = number
+			if count == 4:
+				update_hand_type_on_table(kareta, number)
 
 			elif count == 3:
-				if type_of_hand == 'para':
-					return 'full', value_of_hand * 2 + number * 3, highest_card
+				# if "para" on the table
+				if hand_types_on_table[para]:
+					update_hand_type_on_table(full, number * 3 + value_of_type[para] * 2)
+				# if "trójka" on the table
+				elif hand_types_on_table[trojka]:
+					other_three = value_of_type[trojka]
+					update_hand_type_on_table(full, max(other_three, number) * 3 + min(other_three, number) * 2)
 				else:
-					type_of_hand = 'trojka'
-					value_of_hand = number
+					update_hand_type_on_table(trojka, number)
 
-			elif count == 4:
-				return 'kareta', number, highest_card
-
-		return type_of_hand, value_of_hand, highest_card
-
-	def value_of_five_cards(self, cards):
-		"""finds type of hand, value of it and highest card player has based on five cards passed as 'cards'. """
-		"""returns value of those five cards based on the evaluation by self.value_of_hand_type method"""
-
-		def check_consecutive_len5(l):
-			"""checks if ordered cards are incrementing by one"""
-			what_it_would_be = list(range(min(l), max(l) + 1))
-			return sorted(l) == what_it_would_be and len(what_it_would_be) == 5
-
-		# we make a list of all cards as numbers based on their value
-		main = [self.CARD_VALUES[number] for number, _ in cards]
-		# if there is an ase we have to look for two separate cases
-		secondary = [] if 14 not in main else [1 if x == 14 else x for x in main]
-
-		highest_card = max(main)
-
-		# we check if there are five cards of the same colour
-		colours_on_table = (colours for _, colours in cards)
-		there_is_colour_of_type = (all(col == colour for col in colours_on_table) for colour in self.COLOURS)
-		colour_on_table = any(there_is_colour_of_type)
-
-		"""We check if there is strit or poker on table"""
-		if check_consecutive_len5(main):
-			if colour_on_table:
-				return self.value_of_hand_type('poker', highest_card)
-			else:
-				return self.value_of_hand_type('strit', highest_card)
-		if secondary:
-			"""we need to check if there is strit or poker on table provided we count the ase as 1 (if ase on table)"""
-			if check_consecutive_len5(secondary):
-				if colour_on_table:
-					return self.value_of_hand_type('poker', max(secondary))
+			elif count == 2:
+				# if "para" on the table
+				if hand_types_on_table[para]:
+					update_hand_type_on_table(dwie_pary, value_of_type[para] + number)
+				# if "trójka" on the table
+				elif hand_types_on_table[trojka]:
+					update_hand_type_on_table(full, value_of_type[trojka] * 3 + number * 2)
 				else:
-					return self.value_of_hand_type('strit', max(secondary))
+					update_hand_type_on_table(para, number)
 
-		"""if there are 5 cards of the same colour and method has not terminated by this point 
-		we return 'colour' as the highest type of hand one can have'"""
-		if colour_on_table:
-			return self.value_of_hand_type('kolor', highest_card, highest_card)
-
-		type_of_hand = 'highest_card'
-		value_of_hand = 0
-
-		for number in main:
-			count = main.count(number)
-
-			if count == 2:
-				if type_of_hand == 'para':
-					return self.value_of_hand_type('dwie_pary', value_of_hand * 2 + number * 2, highest_card)
-				elif type_of_hand == 'trojka':
-					return self.value_of_hand_type('full', value_of_hand * 3 + number * 2, highest_card)
-				else:
-					type_of_hand = 'para'
-					value_of_hand = number
-
-			elif count == 3:
-				if type_of_hand == 'para':
-					return self.value_of_hand_type('full', value_of_hand * 2 + number * 3, highest_card)
-				else:
-					type_of_hand = 'trojka'
-					value_of_hand = number
-
-			elif count == 4:
-				return self.value_of_hand_type('kareta', number, highest_card)
-
-		return self.value_of_hand_type(type_of_hand, value_of_hand, highest_card)
-
-	def value_of_hand_type(self, hand_type, value_of_hand_type, max_card=0):
-		"""based on th type of hand this method gives a value of that"""
-		if hand_type != 'highest_card':
-			return 1000 * self.HAND_VALUES.index(hand_type) + 10 * value_of_hand_type + max_card
-		else:
-			return max_card
+		len_hand_values = len(self.HAND_VALUES)
+		for i in range(len_hand_values):
+			current_index = len_hand_values-i-1
+			if hand_types_on_table[current_index]:
+				return current_index, value_of_type[current_index], highest_card
 
 
 if __name__ == '__main__':
 	hand = {('A', 'h'), ('A', 's')}
-	base = {('3', 'h'), ('3', 's'), ('3', 'd'), ('A', 'd'), ('A', 'c')}
+	base = {('3', 'h'), ('3', 's'), ('3', 'd')}
 
+	s = time()
 	evaluator = PokerEvaluator()
 	evaluator.update_table(base)
 	evaluator.update_hand(hand)
-	evaluator.calculate_position()
+	result = evaluator.calculate_position()
+	took = time()-s
+	print('took:', took, 'minutes:', took/60, 'with multiprocessing it would take about:', took/4)
